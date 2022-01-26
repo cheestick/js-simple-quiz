c6123f789b1899d8c2d8b03af9f6278426aa9281
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-01-26 19:35:28 +0200

    main variables and elements references was added. clearPage function too

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
 const questions = [
-	{
-		question: "Какой язык работает в браузере?",
-		answers: ["Java", "C", "Python", "JavaScript"],
-		correct: 4,
-	},
-	{
-		question: "Что означает CSS?",
-		answers: [
-			"Central Style Sheets",
-			"Cascading Style Sheets",
-			"Cascading Simple Sheets",
-			"Cars SUVs Sailboats",
-		],
-		correct: 2,
-	},
-	{
-		question: "Что означает HTML?",
-		answers: [
-			"Hypertext Markup Language",
-			"Hypertext Markdown Language",
-			"Hyperloop Machine Language",
-			"Helicopters Terminals Motorboats Lamborginis",
-		],
-		correct: 1,
-	},
-	{
-		question: "В каком году был создан JavaScript?",
-		answers: ["1996", "1995", "1994", "все ответы неверные"],
-		correct: 2,
-	},
+  {
+    question: "Какой язык работает в браузере?",
+    answers: ["Java", "C", "Python", "JavaScript"],
+    correct: 4,
+  },
+  {
+    question: "Что означает CSS?",
+    answers: [
+      "Central Style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats",
+    ],
+    correct: 2,
+  },
+  {
+    question: "Что означает HTML?",
+    answers: [
+      "Hypertext Markup Language",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Terminals Motorboats Lamborginis",
+    ],
+    correct: 1,
+  },
+  {
+    question: "В каком году был создан JavaScript?",
+    answers: ["1996", "1995", "1994", "все ответы неверные"],
+    correct: 2,
+  },
 ];
+
+// Search elements
+
+const headerContainer = document.querySelector("#header");
+const listContainer = document.querySelector("#list");
+const submitBtn = document.querySelector("#submit");
+
+// game variables
+let score = 0; // right aswers quantity
+let questionIndex = 0; // current question
+
+clearPage();
+
+// clear html
+function clearPage() {
+  headerContainer.innerHTML = "";
+  listContainer.innerHTML = "";
+}
